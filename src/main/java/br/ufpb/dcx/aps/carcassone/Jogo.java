f45b0c3f5eded5c5d492e0c37d6a748b19d130c9
Rodrigo Vilar <rvilar@gmail.com>
2018-04-13 06:37:36 -0300

    Unidade 2 v1

--- src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
@@ -1,82 +1,11 @@
 package br.ufpb.dcx.aps.carcassone;
 
-import br.ufpb.dcx.aps.carcassone.tabuleiro.TabuleiroFlexivel;
-import br.ufpb.dcx.aps.carcassone.tabuleiro.Tile;
-
 public class Jogo {
-	
-	private Tile proximoTile;
-	private BolsaDeTiles tiles;
-	private TabuleiroFlexivel tabuleiro = new TabuleiroFlexivel("  ");
-	
-	
-	public Jogo(BolsaDeTiles tiles) {
-		this.tiles = tiles;
+
+	public Partida criarPartida(BolsaDeTiles tiles, Cor... sequencia) {
+		return new Partida(tiles);
 	}
 
-	public Jogo iniciarPartida(Cor... sequencia) {
-		pegarProximoTile();
-		return this;
-	}
 
-	public String relatorioPartida() {
-		return null;
-	}
-	
-	public Jogo girarTile() {		
-		proximoTile.girar();
-		return this;
-	}
 
-	public Jogo posicionarInicial() {
-		tabuleiro.adicionarPrimeiroTile(proximoTile);
-		return this;
-	}
-
-	private void pegarProximoTile() {
-		proximoTile = tiles.pegar();
-		proximoTile.reset();
-	}
-
-	public Jogo finalizarRodada() {
-		pegarProximoTile();
-		return this;
-	}
-
-	public Jogo posicionarTile(Tile tileReferencia, Lado ladoTileReferencia) {
-		tabuleiro.posicionar(tileReferencia, ladoTileReferencia, proximoTile);
-		return this;		
-	}
-	
-	public Jogo posicionarMeepleEstrada(Lado lado) {
-		return this;
-	}
-	
-	public Jogo posicionarMeepleCampo(Vertice vertice) {
-		return this;
-	}
-	
-	public Jogo posicionarMeepleCidade(Lado lado) {
-		return this;
-	}
-	
-	public Jogo posicionarMeepleMosteiro() {
-		return this;
-	}
-	
-	public String getEstradas() {
-		return null;
-	}
-	
-	public String getCampos() {
-		return null;
-	}
-	
-	public String getCidades() {
-		return null;
-	}
-
-	public String getMosteiros() {
-		return null;
-	}
 }
